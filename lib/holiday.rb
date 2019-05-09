01b605fad6e6df0875453f727ba2d160347fdfca
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 22:33:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,9 +62,13 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holiday_hash.collect do |season, holiday|
+    holiday.collect do |holiday, supplies|
+        if supplies.include?("BBQ")
+             holiday
+      end
+    end.compact
+  end.flatten
 end
 
 
