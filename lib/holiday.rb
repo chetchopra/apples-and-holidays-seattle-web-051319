77ef45e253a097f8e0beefbcfc5543110b35afee
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 22:31:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     return holiday_hash
 end
 
+
+
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
